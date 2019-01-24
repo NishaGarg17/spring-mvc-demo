41a024f7bc1068559961dbedf3bc1699d24dfea7
Nisha <14gargnisha>
2019-01-24 09:38:55 +0530

    v5

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/model/Customer.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/model/Customer.java
@@ -1,6 +1,9 @@
 package com.luv2code.springdemo.mvc.model;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
@@ -9,6 +12,28 @@ public class Customer {
 	@NotNull(message="is required")
 	@Size(min=1,message="is required")
 	private String lastName;
+	
+	@NotNull(message="is required")
+	@Min(value=0,message="must be greater than or equal to 0")
+	@Max(value=10,message="must be less than or equal to 10")
+	private Integer freePasses;
+	
+	@Pattern(regexp="^[A-Za-z0-9]{5}",message="only 5 chars/digits")
+	private String postalCode; 
+	
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	public Integer getFreePasses() {
+		return freePasses;
+	}
+	public void setFreePasses(Integer freePasses) {
+		this.freePasses = freePasses;
+	}
+
 	public String getFirstName() {
 		return firstName;
 	}
